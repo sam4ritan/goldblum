5134ef52aacc2857d55ef0aba07d076e750faae6
Ben Darlow <github@kapowaz.net>
2017-06-27 23:24:03 +0100

    Tidy up removing some temporary info

--- app/routes/routes.rb
+++ app/routes/routes.rb
@@ -22,29 +22,12 @@ class Goldblum < Sinatra::Base
   end
 
   post '/invoke' do
-    # example posted data
-    # token=gIkuvaNzQIHg97ATvDxqgjtO
-    # team_id=T0001
-    # team_domain=example
-    # enterprise_id=E0001
-    # enterprise_name=Globular%20Construct%20Inc
-    # channel_id=C2147483705
-    # channel_name=test
-    # user_id=U2147483697
-    # user_name=Steve
-    # command=/weather
-    # text=94070
-    # response_url=https://hooks.slack.com/commands/1234/5678
-
-    callback_id = "goldblum_#{Time.now.to_i.base62_encode}"
-
     {
-      # "response_type": "in_channel",
       "text": "Initiate Goldblum protocol?",
       "attachments": [
         {
           "fallback": "You are unable to Goldblum :sob:",
-          "callback_id": callback_id,
+          "callback_id": "goldblum_#{Time.now.to_i.base62_encode}",
           "color": "#ff0000",
           "attachment_type": "default",
           "actions": [
@@ -61,10 +44,10 @@ class Goldblum < Sinatra::Base
               "type": "button",
               "value": "proceed",
               "confirm": {
-                "title": "Execute Goldblum protocol on this user",
+                "title": "Initiate Goldblum protocol on this user",
                 "text": "Are you sure?",
                 "ok_text": "Yes, do it!",
-                "dismiss_text": "On second thoughts, no"
+                "dismiss_text": "On second thoughts…"
               }
             }
           ]
@@ -74,38 +57,6 @@ class Goldblum < Sinatra::Base
   end
 
   post '/goldblum' do
-    # TODO: message button action endpoint
-
-    # example JSON payload submitted to this endpoint
-    # {
-    #   "actions": [
-    #     {
-    #       "name": "recommend",
-    #       "value": "yes",
-    #       "type": "button"
-    #     }
-    #   ],
-    #   "callback_id": "comic_1234_xyz",
-    #   "team": {
-    #     "id": "T47563693",
-    #     "domain": "watermelonsugar"
-    #   },
-    #   "channel": {
-    #     "id": "C065W1189",
-    #     "name": "forgotten-works"
-    #   },
-    #   "user": {
-    #     "id": "U045VRZFT",
-    #     "name": "brautigan"
-    #   },
-    #   "action_ts": "1458170917.164398",
-    #   "message_ts": "1458170866.000004",
-    #   "attachment_id": "1",
-    #   "token": "xAB3yVzGS4BQ3O9FACTa8Ho4",
-    #   "original_message": {"text":"New comic book alert!","attachments":[{"title":"The Further Adventures of Slackbot","fields":[{"title":"Volume","value":"1","short":true},{"title":"Issue","value":"3","short":true}],"author_name":"Stanford S. Strickland","author_icon":"https://api.slack.comhttps://a.slack-edge.com/bfaba/img/api/homepage_custom_integrations-2x.png","image_url":"http://i.imgur.com/OJkaVOI.jpg?1"},{"title":"Synopsis","text":"After @episod pushed exciting changes to a devious new branch back in Issue 1, Slackbot notifies @don about an unexpected deploy..."},{"fallback":"Would you recommend it to customers?","title":"Would you recommend it to customers?","callback_id":"comic_1234_xyz","color":"#3AA3E3","attachment_type":"default","actions":[{"name":"recommend","text":"Recommend","type":"button","value":"recommend"},{"name":"no","text":"No","type":"button","value":"bad"}]}]},
-    #   "response_url": "https://hooks.slack.com/actions/T47563693/6204672533/x7ZLaiVMoECAW50Gw1ZYAXEM"
-    # }
-
     payload       = JSON.parse(URI.decode(params['payload']))
     action_value  = payload['actions'].first['value']
     channel_id    = payload['channel']['id']
