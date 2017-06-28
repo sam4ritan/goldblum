d5759e8a94b46d8d5f815220b1f9baef5c32793f
Ben Darlow <github@kapowaz.net>
2017-06-28 10:40:30 +0100

    Move avatars into a public directory so they can be accessed from the web

--- app/lib/avatar.rb
+++ app/lib/avatar.rb
@@ -2,7 +2,7 @@ class Goldblum
   class Avatar
     def self.get
       avatar_number = rand(1..Goldblum.settings.avatar_count)
-      "app/avatars/#{sprintf("%03d", avatar_number)}.jpg"
+      "public/avatars/#{sprintf("%03d", avatar_number)}.jpg"
     end
   end
 end
