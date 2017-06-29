cbf3482a0f6645e0607274e3241cc5d101f9e346
Ben Darlow <github@kapowaz.net>
2017-06-29 14:35:59 +0100

    Fix avatar path in sample email page

--- app/routes/email.rb
+++ app/routes/email.rb
@@ -3,7 +3,7 @@ class Goldblum < Sinatra::Base
     content_type 'text/html'
 
     erb :email, locals: {
-      image_url: 'https://goldblum.herokuapp.com/avatars/ianmalcolm/004.jpg',
+      image_url: 'avatars/ianmalcolm/004.jpg',
       hero_heading: 'Ian Malcolm',
       hero_text: '“Your scientists were so preoccupied with whether or not they could, they didn’t stop to think if they should.”',
     }
